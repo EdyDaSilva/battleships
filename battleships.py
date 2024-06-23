a6d0c8549e53f90120c93e285ed4f7bee08961d9
EdyDaSilva <ednedson@hotmail.com>
2024-06-23 15:49:34 +0100

    Place ships on grid

--- battleships.py
+++ battleships.py
@@ -9,3 +9,15 @@ def print_grid(grid):
         print(" ".join(row))
     print()
 
+
+def place_ship(grid, size, ship_count):
+    ships = []
+    for _ in range(ship_count):
+        while True:
+            ship_row = random.randint(0, size-1)
+            ship_col = random.randint(0, size-1)
+            if (ship_row, ship_col) not in ships:
+                ships.append((ship_row, ship_col))
+                break
+    return ships
+
