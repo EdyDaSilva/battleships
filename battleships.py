ca2d6d8deb26ece3c710557b628c47c9d03db636
EdyDaSilva <ednedson@hotmail.com>
2024-06-23 15:48:48 +0100

    Function for grid

--- battleships.py
+++ battleships.py
@@ -2,3 +2,10 @@ import random
 
 def initialize_grid(size):
     return [["~" for _ in range(size)] for _ in range(size)]
+
+
+def print_grid(grid):
+    for row in grid:
+        print(" ".join(row))
+    print()
+
